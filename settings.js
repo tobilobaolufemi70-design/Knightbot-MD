2b2772b29d98a7868a3e3d28015586fb3620f87f
tobilobaolufemi70-design <tobilobaolufemi70@gmail.com>
2026-02-13 23:58:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'chris Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "chris Bot",
+  botOwner: 'Chris', // Your name
+  ownerNumber: '2347073793930', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
